fe727855bf1e440ad1ca9003244e8b7f0dac2673
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-09-16 17:44:26 +0200

    tolgo commenti

--- src/main/java/kassandrafalsitta/u2w3d1/services/AuthService.java
+++ src/main/java/kassandrafalsitta/u2w3d1/services/AuthService.java
@@ -16,15 +16,11 @@ public class AuthService {
     private JWTTools jwtTools;
 
     public String checkCredentialsAndGenerateToken(EmployeeLoginDTO body) {
-        // 1. Controllo le credenziali
-        // 1.1 Cerco nel db tramite email se esiste l'utente
+
         Employee found = this.employeesService.findByEmail(body.email());
         if (found.getPassword().equals(body.password())) {
-            // 1.2 Se lo trovo verifico se la pw trovata combacia con quella passataci tramite body
-            // 2. Se è tutto ok --> genero un access token e lo restituisco
             return jwtTools.createToken(found);
         } else {
-            // 3. Se le credenziali sono errate --> 401 (Unauthorized)
             throw new UnauthorizedException("Credenziali errate!");
         }
 
